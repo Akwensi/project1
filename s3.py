3206316c54d105e6ea99fe1b89f8c310cc7f5f4b
Akwensi <justiceakwensi@gmail.com>
2019-10-08 07:18:52 +0000

    edited key name

---
+++ s3.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 21:11:31 2019
+
+@author: user
+"""
+import boto3
+import botocore
+import pandas as pd
+
+BUCKET_NAME = 'blossom-data-engs'
+KEY = 'free-7-million-company-dataset.zip'
+
+s3 = boto3.resource('s3')
+
+try:
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 'my_free-7-million-company-dataset.zip')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
+
+# read with pandas  
+df = pd.read_csv('my_free-7-million-company-dataset.zip')
+
+# setting na_values
+df = pd.read_csv('my_free-7-million-company-dataset.zip', na_values = {
+    'domain': ["not available", "n.a", "n.a."})
+                 
+#filter off companies with empty domain 
+df = df.dropna(subset = ['domain'])
+
+#writing to parquet format 
+df.to_parquet('my_free-7-million-company-dataset.parquet.gzip', compression='gzip')
+                 
+#writing to json format
+df.to_json(r'my_free-7-million-company-dataset.json')
+
+
+s3_client.upload_file("my_free-7-million-company-dataset.zip", blossom-data-eng-justice-akwensi/project1, "free-7-million-company-dataset-json.gzip")
+
+s3_client.upload_file("my_free-7-million-company-dataset.zip", blossom-data-eng-justice-akwensi/project1, "free-7-million-company-dataset-json.gzip")
